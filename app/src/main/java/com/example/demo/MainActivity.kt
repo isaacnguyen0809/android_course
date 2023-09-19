8e56be0cba02de8a5448a8dd08263143c2b2d726
hoangchungk53qx1 <hoangchungk53qx1@gmail.com>
2023-09-19 21:36:48 +0700

    activity

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,11 +1,88 @@
 package com.example.demo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private var counter: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvClick.setOnClickListener {
+            val data: String = "This is the message from MainActivity"
+            val intent = Intent(this, SecondActivity::class.java).apply {
+                putExtra("message", data)
+            }
+//            startActivity(intent)
+//            finish()
+            resultLauncher.launch(intent)
+//            counter++
+//            binding.tvCounter.text = counter.toString()
+        }
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt(KEY_COUNTER, 0)
+            binding.tvCounter.text = counter.toString()
+        }
+        Log.d(TAG, "onCreate: ")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_COUNTER, counter)
+    }
+
+    private val resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val intentData = result.data
+                val message = intentData?.getStringExtra("result")
+//                binding.tvMain.text = message
+                Toast.makeText(this, "Result: $message", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: ")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val KEY_COUNTER = "KEY_COUNTER"
     }
 }
---
+++ app/src/main/java/com/example/demo/SecondActivity.kt
@@ -0,0 +1,72 @@
+package com.example.demo
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.demo.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private val binding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivitySecondBinding.inflate(
+            layoutInflater
+        )
+    }
+    private var counter: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val mess = intent.getStringExtra("message")
+        val data: String = "This is the message from SecondActivity"
+        binding.tvSecond.setOnClickListener {
+//            val resultIntent = Intent().apply {
+//                putExtra("result", data)
+//            }
+//            setResult(RESULT_OK, resultIntent)
+//            finish()
+            counter++
+            binding.tvSecond.text = counter.toString()
+        }
+        Log.d(TAG, "onCreate: $mess")
+
+        finish()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: ")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+    }
+
+    companion object {
+        private const val TAG = "SecondActivity"
+    }
+
+}
