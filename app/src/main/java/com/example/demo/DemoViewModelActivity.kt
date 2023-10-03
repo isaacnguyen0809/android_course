4a3c103c501997dce6b1e352cf75febd88f9ef56
hoangchungk53qx1 <hoangchungk53qx1@gmail.com>
2023-10-03 22:27:24 +0700

    part 7 viewmodel ,livedata

--- app/src/main/java/com/example/demo/DemoViewModelActivity.kt
+++ app/src/main/java/com/example/demo/DemoViewModelActivity.kt
@@ -1,14 +1,22 @@
 package com.example.demo
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.demo.databinding.ActivityDemoViewModelBinding
+import com.example.demo.localfile.LocalFileViewModel
 import com.example.demo.viewmodel.DemoViewModel
+import android.Manifest
 import com.example.demo.viewmodel.DemoViewModelFactory
 
 class DemoViewModelActivity : AppCompatActivity() {
@@ -23,16 +31,47 @@ class DemoViewModelActivity : AppCompatActivity() {
         }
     )
 
+    private val viewModelLocalFile by viewModels<LocalFileViewModel>()
+
     private val binding by lazy {
         ActivityDemoViewModelBinding.inflate(layoutInflater)
     }
+
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) {
+                Toast.makeText(this, "Da co quyen", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Chua co quyen", Toast.LENGTH_LONG).show()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         binding.tvClick.setOnClickListener {
-            simpleViewModel.counter()
+//            simpleViewModel.counter()
+            requestPermission.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+
+        binding.tvClickSaveImage.setOnClickListener {
+            Glide.with(this)
+                .asBitmap()
+                .load(R.drawable.logo)
+                .into(object : CustomTarget<Bitmap?>() {
+                    override fun onResourceReady(
+                        resource: Bitmap,
+                        transition: Transition<in Bitmap?>?
+                    ) {
+                        viewModelLocalFile.saveImageExternal(resource)
+                    }
+
+                    override fun onLoadCleared(placeholder: Drawable?) {
+
+                    }
+
+                })
         }
 
         showText()
---
+++ app/src/main/java/com/example/demo/localfile/LocalFileViewModel.kt
@@ -0,0 +1,76 @@
+package com.example.demo.localfile
+
+import android.app.Application
+import android.content.ContentValues
+import android.graphics.Bitmap
+import android.media.MediaScannerConnection
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.lifecycle.AndroidViewModel
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+
+class LocalFileViewModel(private val application: Application) : AndroidViewModel(application) {
+
+    private val internalPath = application.getExternalFilesDir(null as String?)
+    private val temptFolderImage = "$internalPath/temptFolderImage"
+
+    private val ANDROID_DIRECTORY = "Android_003"
+
+    val ANDROID_RELATIVE_PATH =
+        "${Environment.DIRECTORY_PICTURES}${File.separator}$ANDROID_DIRECTORY"
+
+    val ANDROID_LEGACY_PATH =
+        "${Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)}${File.separator}$ANDROID_DIRECTORY"
+
+    fun saveImageInternal(bitmap: Bitmap) {
+        val tempDataFolder = temptFolderImage
+        val folderData = File(tempDataFolder)
+        if (!folderData.exists()) {
+            folderData.mkdirs()
+        }
+        val outFile = File("$tempDataFolder/${System.currentTimeMillis()}.jpg")
+        val fos = FileOutputStream(outFile)
+        fos.use {
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+        }
+    }
+
+    fun saveImageExternal(bitmap: Bitmap) {
+        val fileName = "${System.currentTimeMillis()}"
+        var fos: OutputStream? = null
+        var uri: Uri? = null
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            application.applicationContext.contentResolver?.also { resolver ->
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, ANDROID_RELATIVE_PATH)
+                }
+                val imageUri = resolver.insert(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    contentValues
+                )
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+                uri = imageUri
+            }
+        } else {
+            val directory = File(ANDROID_LEGACY_PATH)
+            if (!directory.exists()) {
+                directory.mkdirs()
+            }
+            val image = File(ANDROID_LEGACY_PATH, fileName)
+            MediaScannerConnection.scanFile(
+                application.applicationContext, arrayOf(image.absolutePath),
+                arrayOf("image/jpeg"), null
+            )
+            fos = FileOutputStream(image)
+            fos?.use {
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            }
+        }
+    }
+}
