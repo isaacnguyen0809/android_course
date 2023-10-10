a930bd0bb328125874b8573b9df950dd55c8d1be
hoc081098@gmail.com <hoc081098@gmail.com>
2023-10-10 17:28:48 +0700

    basic lec9 cont wip

--- app/src/main/java/com/example/demo/unsplash/feature/search/SearchFragment.kt
+++ app/src/main/java/com/example/demo/unsplash/feature/search/SearchFragment.kt
@@ -3,6 +3,4 @@ package com.example.demo.unsplash.feature.search
 import com.example.demo.databinding.FragmentSearchBinding
 import com.example.demo.unsplash.core.BaseFragment
 
-class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding::inflate) {
-
-}
+class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding::inflate)
